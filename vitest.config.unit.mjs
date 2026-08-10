c18c83a7c4d67279f3c6249fc0db52c20dee02cb
dieh <dri.leela@gmail.com>
2026-08-10 09:34:33 -0300

    fix sonar configuration

--- vitest.config.unit.mjs
+++ vitest.config.unit.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 		include: ["src/**/*.{test,spec}.{ts,tsx}"],
 		coverage: {
 			provider: "v8",
-			reporter: ["text", "html"],
+			reporter: ["text", "html", "lcov"],
 			exclude: ["node_modules/", "src/generated/**"],
 		},
 	},
